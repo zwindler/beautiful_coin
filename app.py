dee83894e280506b49079ea09312af3d9af28c49
Denis GERMAIN <dt.germain@gmail.com>
2024-12-22 14:26:00 +0100

    chore: WIP 2 sides of the coin

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, jsonify
 import modules.elements as elements
 
 app = Flask(__name__)
@@ -10,18 +10,35 @@ def index():
 
 @app.route("/generate", methods=["POST"])
 def generate():
-    """Generate coat of arms and coin based on selected crown."""
-    crown_path = request.form["crown"]
-    shield_path = request.form["shield"]
-    laurels_path = request.form["sides"]
+    """Generate coat of arms and coin based on selection in UI."""
+    heads_crown_path = request.form["crown"]
+    heads_icon_path = request.form["icon-heads"]
+    heads_laurels_path = request.form["sides-heads"]
+    tails_crown_path = request.form["crown-tails"]
+    tails_shield_path = request.form["shield-tails"]
+    tails_laurels_path = request.form["sides-tails"]
+
+    # Paths for output files
+    heads_svg = "output/coin-heads.svg"
+    tails_svg = "output/coin-tails.svg"
     coat_of_arms_path = "output/coat_of_arms.svg"
-    coin_output_path = "output/coin.svg"
 
-    # Call functions to generate SVGs
-    elements.create_coat_of_arms(coat_of_arms_path, shield_path)
-    elements.create_coin(coin_output_path, coat_of_arms_path, crown_path, laurels_path)
+    # Heads always have a single SVG as "head"
+    elements.create_coin(heads_svg, heads_icon_path, heads_crown_path, heads_laurels_path)
+    # Tails always have a coat_of_arms
+    elements.create_coat_of_arms(coat_of_arms_path, tails_shield_path)
+    elements.create_coin(tails_svg, coat_of_arms_path, tails_crown_path, tails_laurels_path)
 
-    return send_file(coin_output_path, mimetype="image/svg+xml")
+    with open(heads_svg, "r") as f:
+        heads_svg_content = f.read()
+
+    with open(tails_svg, "r") as f:
+        tails_svg_content = f.read()
+
+    return jsonify({
+        "heads": heads_svg_content,
+        "tails": tails_svg_content
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
